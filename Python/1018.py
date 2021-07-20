b737942655629087c84de4d092f98880c659759a
SongSiWoon <siwoons7319@gmail.com>
2021-07-20 22:16:46 +0900

    brute force

---
+++ Python/1018.py
@@ -0,0 +1,26 @@
+import sys
+n, m = map(int,input().split())
+bd = [list(sys.stdin.readline().rstrip()) for i in range(n)]
+
+result = []
+for i in range(n-7):
+    for j in range(m-7):
+        cw = 0
+        cb = 0
+        for a in range(i,i+8):
+            for b in range(j,j+8):
+                if (a+b)%2 ==0:
+                    if bd[a][b] != 'W':
+                            cw += 1
+                    if bd[a][b] != 'B':
+                            cb += 1
+
+                else:
+                    if bd[a][b] != 'B':
+                            cw += 1
+                    if bd[a][b] != 'W':
+                            cb += 1
+
+        result.append(min(cw,cb))
+
+print(min(result))
