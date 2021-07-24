c1824658cc2d75317b0e150fe100fb1c0bd1ef34
SongSiWoon <siwoons7319@gmail.com>
2021-07-24 23:39:15 +0900

    deque

---
+++ Python/10866.py
@@ -0,0 +1,43 @@
+import sys
+from collections import deque
+
+deq = deque()
+
+n = int(input())
+
+for i in range(n):
+    str = sys.stdin.readline().strip().split()
+
+    if str[0] == 'push_front':
+        deq.appendleft(str[1])
+    elif str[0] == 'push_back':
+        deq.append(str[1])
+    elif str[0] == 'pop_front':
+        if len(deq)==0:
+            print(-1)
+        else:
+            print(deq.popleft())
+    elif str[0] == 'pop_back':
+        if len(deq)==0:
+            print(-1)
+        else:
+            print(deq.pop())
+    elif str[0] == 'size':
+        print(len(deq))
+    elif str[0] == 'empty':
+        if len(deq) == 0:
+            print(1)
+        else:
+            print(0)
+    elif str[0] == 'front':
+        if len(deq)==0:
+            print(-1)
+        else:
+            print(deq[0])
+    elif str[0] == 'back':
+        if len(deq)==0:
+            print(-1)
+        else:
+            print(deq[-1])
+
+            
