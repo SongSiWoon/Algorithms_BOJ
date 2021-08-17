9d728e7d82c41df8708177a3d0256b0c2281c38a
SongSiWoon <siwoons7319@gmail.com>
2021-08-17 10:30:40 +0900

    greedy

---
+++ Python/1339.py
@@ -0,0 +1,28 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+str = []
+for i in range(n):
+    str.append(input().rstrip())
+print(str)
+dic = {}
+for s in str:
+    l = len(s)-1
+    for i in s:
+        if i in dic:
+            dic[i] += (10 ** l)
+        else:
+            dic[i] = (10 ** l)
+        l -= 1
+
+num = []
+for i in dic.values():
+    num.append(i)
+num.sort(reverse=True)
+result =0
+scale = 9
+for i in range(len(num)):
+    result += num[i]*scale
+    scale -= 1
+print(result)
