0dc068c56138c8815d596741d102711b0a39dc5c
SongSiWoon <siwoons7319@gmail.com>
2021-08-20 16:49:43 +0900

    greedy

---
+++ Python/16953.py
@@ -0,0 +1,13 @@
+a, b = map(int, input().split())
+cnt = 1
+while a<b:
+    if b%2 == 0:
+        b = b//2
+        cnt += 1
+    elif b%10 == 1:
+        b = b//10
+        cnt+=1
+if a == b:
+    print(cnt)
+else:
+    print(-1)
