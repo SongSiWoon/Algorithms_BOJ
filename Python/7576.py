344b14a88ad6e2663b731d681583ede6d4c27b89
SongSiWoon <siwoons7319@gmail.com>
2021-08-27 15:44:34 +0900

    BFS

--- Python/7576.py
+++ Python/7576.py
@@ -1,7 +1,7 @@
 from collections import deque
 m, n = map(int, input().split())
 matrix = [list(map(int, input().split())) for i in range(n)]
-day = [[0]*m]*n
+#day = [[0]*m]*n
 dx = [0,0,-1,1]
 dy = [-1,1,0,0]
 cnt = 0
@@ -10,22 +10,25 @@ for i in range(n):
     for j in range(m):
         if matrix[i][j] == 1:
             q.append([i,j])
+            #day[i][j] = 1
 def bfs():
     global cnt
     while q:
         cnt+=1
-        qx, qy = q.popleft()
-        for i in range(4):
-            nx = qx+dx[i]
-            ny = qy+dy[i]
-            if 0<=nx<n and 0<=ny<m:
-                if matrix[nx][ny] == 0:
-                    matrix[nx][ny] = 1
-                    day[nx][ny] = day[qx][qy] +1
-                    q.append([nx,ny])
+        for l in range(len(q)):
+            qx, qy = q.popleft()
+            for i in range(4):
+                nx = qx+dx[i]
+                ny = qy+dy[i]
+                if 0<=nx<n and 0<=ny<m:
+                    if matrix[nx][ny] == 0:
+                        matrix[nx][ny] = 1
+                        #day[nx][ny] = day[qx][qy] +1
+                        q.append([nx,ny])
     for i in range(n):
         for j in range(m):
             if matrix[i][j] == 0:
                 return -1
-    return max(max(day))
+    return cnt-1
+    #return max(max(day))
 print(bfs())
