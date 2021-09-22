d297dfb59b45e64a07e4877782584925a9d6eb5b
SongSiWoon <siwoons7319@gmail.com>
2021-09-22 21:43:59 +0900

    changed

--- Python/13171.py
+++ Python/13171.py
@@ -9,18 +9,3 @@ def power(a, n):
         return x * x % mod
     return (a * power(a, n-1))%mod
 print(power(a,x))
-
-
-MOD = 1_000_000_007
-
-
-def pow(n, m):
-    if m == 1:
-        return n
-    else:
-        ret = pow(n, m // 2)
-        powerd = ret * ret % MOD
-        if m % 2 == 0:
-            return powerd % MOD
-        else:
-            return (powerd * n) % MOD
