7d5088243e2df97eb48020e6dc760064f499e36c
SongSiWoon <siwoons7319@gmail.com>
2021-12-24 23:45:14 +0900

    brakcets

--- Python/2504.py
+++ Python/2504.py
@@ -1,50 +1,64 @@
-stack = []
-res = 0
-li = list(input())
-for i in li:
-    if i == ')':
-        tmp = 0
-        while stack:
-            ret = stack.pop()
-            if ret == '(':
-                if tmp == 0:
-                    stack.append(2)
-                else:
-                    stack.append(2*tmp)
-                break
-            elif ret == '[':
-                print(0)
-                exit()
-            else:
-                if tmp == 0:
-                    tmp = int(ret)
-                else:
-                    tmp += int(ret)
-    elif i == ']':
-        tmp = 0
-        while stack:
-            ret = stack.pop()
-            if ret == '[':
-                if tmp == 0:
-                    stack.append(3)
-                else:
-                    stack.append(3*tmp)
-                break
-            elif ret == '(':
-                print(0)
-                exit()
-            else:
-                if tmp == 0:
-                    tmp = int(ret)
-                else:
-                    tmp += int(ret)
-    else:
-        stack.append(i)
+brakcets = list(input().strip())
 
-for i in stack:
-    if i == '(' or i == '[':
-        print(0)
-        exit()
+
+def check(brakcets):
+    stack = []
+    for b in brakcets:
+        if b in ('(', '['):
+            stack.append(b)
+        elif b in (')', ']'):
+            if len(stack) == 0:
+                return False
+            tmp = stack.pop()
+            if b == ')':
+                if tmp != '(':
+                    return False
+            elif b == ']':
+                if tmp != '[':
+                    return False
+    return False if stack else True
+
+
+def sol(brakcets):
+    stack = []
+    for b in brakcets:
+        if b in ('(', '['):
+            stack.append(b)
+        elif b == ')':
+            tmp = 0
+            while stack:
+                top = stack.pop()
+                if top == '(':
+                    if tmp == 0:
+                        stack.append(2)
+                    else:
+                        stack.append(2*tmp)
+                    break
+                else:
+                    if tmp == 0:
+                        tmp = int(top)
+                    else:
+                        tmp += int(top)
+        elif b == ']':
+            tmp = 0
+            while stack:
+                top = stack.pop()
+                if top == '[':
+                    if tmp == 0:
+                        stack.append(3)
+                    else:
+                        stack.append(3*tmp)
+                    break
+                else:
+                    if tmp == 0:
+                        tmp = int(top)
+                    else:
+                        tmp += int(top)
+    return sum(stack)
+
+
+if __name__ == "__main__":
+    if check(brakcets):
+        print(sol(brakcets))
     else:
-        res += i
-print(res)
+        print(0)
