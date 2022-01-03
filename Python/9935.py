86daca247686e1830d1139f29a893d703a6d5bd6
SongSiWoon <siwoons7319@gmail.com>
2022-01-03 20:03:58 +0900

    [FIX]: 9935/String

--- Python/9935.py
+++ Python/9935.py
@@ -3,14 +3,19 @@ bomb = input()
 
 
 def sol(String, bomb):
-    ret = String
-    while bomb in ret:
-        tmp = ret.split(bomb)
-        val = ''
-        for s in tmp:
-            val += s
-        ret = val
-    return ret if ret != '' else 'FRULA'
+    stack = []
+    blen = len(bomb)
+    for s in String:
+        stack.append(s)
+        if s == bomb[-1]:
+            tmp = ''
+            for i in stack[-blen:]:
+                tmp += i
+            if tmp == bomb:
+                for i in range(blen):
+                    stack.pop()
+    res = ''.join(stack)
+    return res if res != '' else 'FRULA'
 
 
 print(sol(String, bomb))
