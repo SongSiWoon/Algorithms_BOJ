f26fee4d92d4f218e5f02016813ddf3b0424d56e
SongSiWoon <siwoons7319@gmail.com>
2022-01-13 22:57:41 +0900

    [FEAT]: 1927/Minheap

---
+++ Python/1927.py
@@ -0,0 +1,15 @@
+import heapq
+import sys
+input = sys.stdin.readline
+
+heap = []
+N = int(input())
+for _ in range(N):
+    tmp = int(input())
+    if tmp == 0:
+        if len(heap) == 0:
+            print(0)
+        else:
+            print(heapq.heappop(heap))
+    else:
+        heapq.heappush(heap, tmp)
