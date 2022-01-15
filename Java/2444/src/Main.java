68f40e3b5b8e6d1a210a17e43140e1b83cf85832
SongSiWoon <siwoons7319@gmail.com>
2022-01-15 22:43:39 +0900

    [FIX]: Reform java code

--- Java/2444/src/Main.java
+++ Java/2444/src/Main.java
@@ -9,6 +9,7 @@ public class Main {
     }
 
     public static void PrintStar(int n){
+
         for(int i = 1; i < n+1; i++){
             System.out.println(" ".repeat(n-i) + "*".repeat(i) + "*".repeat(i-1));
         }
